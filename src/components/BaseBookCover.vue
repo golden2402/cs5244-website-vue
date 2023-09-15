be3952c99f1fda8da326801845bfa37ce107fe6e
golden2402 <golden2402@vt.edu>
2023-09-15 12:09:30 -0400

    add featured section, flex util

---
+++ src/components/BaseBookCover.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import BaseCard from "./BaseCard.vue";
+
+  defineProps<{
+    src: string;
+  }>();
+</script>
+
+<template>
+  <BaseCard class="cover__seat">
+    <img :src="src" class="book__cover" height="200" />
+  </BaseCard>
+</template>
+
+<style scoped>
+  .cover__seat {
+    padding: 0.2em;
+    max-width: 150px;
+  }
+</style>
--- src/components/FeatureSection.vue
+++ src/components/FeatureSection.vue
@@ -1,5 +1,48 @@
+<script setup lang="ts">
+  import { inject } from "vue";
+  import type { BookItem } from "@/types";
+
+  import BookCover from "@/components/BaseBookCover.vue";
+
+  import IconStar06 from "@/assets/icons/IconStar06.vue";
+
+  const bookList: Map<number, BookItem> = inject("bookList")!;
+  const featuredBooks = [1001, 1001, 1001, 1001, 1001, 1001].map((id) => bookList.get(id)).filter(Boolean);
+</script>
+
 <template>
-  <main></main>
+  <div class="featured flex flex--column justify--center gap--md">
+    <div>
+      <div class="featured__header flex justify--center align--center gap--xs">
+        <IconStar06 />
+        <h1 class="featured__title">Featured Titles</h1>
+      </div>
+      <p class="featured__blurb">Your next best book could be right here.</p>
+    </div>
+    <div v-if="featuredBooks.length > 0" class="flex flex--wrap justify--center gap--sm">
+      <BookCover v-for="(item, id) of featuredBooks" v-bind:key="id" :src="item!.cover" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .featured {
+    height: 100%;
+  }
+
+  .featured__header {
+    color: var(--primary-color);
+
+    font-size: 1.35em;
+    line-height: 1.2;
+  }
+
+  .featured__header .featured__title {
+    text-transform: uppercase;
+  }
+
+  .featured__blurb {
+    text-align: center;
+    font-size: 1.1em;
+  }
+</style>
