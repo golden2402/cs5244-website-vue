e057ed5ed7138f5e6f803ed553b4b4f1122eae96
golden2402 <golden2402@vt.edu>
2023-09-23 00:35:38 -0400

    add read now button

--- src/components/CategorySelection.vue
+++ src/components/CategorySelection.vue
@@ -6,6 +6,7 @@
   import BaseBookCover from "@/components/BaseBookCover.vue";
   import BaseCard from "@/components/BaseCard.vue";
 
+  import IconBookOpen01 from "@/assets/icons/IconBookOpen01.vue";
   import IconShoppingCart02 from "@/assets/icons/IconShoppingCart02.vue";
 
   defineProps<{
@@ -17,7 +18,14 @@
   <!-- sort of like sinking the error; maybe bad practice: -->
   <BaseCard class="category__item">
     <div class="flex gap--sm">
-      <BaseBookCover class="book__cover" :src="book.cover" />
+      <div class="book__cover__seat">
+        <BaseBookCover class="book__cover" :src="book.cover" />
+        <RouterLink to="" v-if="book.isPublic">
+          <div class="read__now__button box--secondary">
+            <IconBookOpen01 />
+          </div>
+        </RouterLink>
+      </div>
       <div class="category__item__details flex flex--column justify--between">
         <section class="book__header">
           <h3>{{ book.title }}</h3>
@@ -44,6 +52,26 @@
   .category__item {
     height: max-content;
     width: 440px;
+  }
+
+  .book__cover {
+    width: 150px;
+  }
+
+  .book__cover__seat {
+    position: relative;
+  }
+
+  .read__now__button {
+    display: inline-flex;
+    
+    position: absolute;
+    bottom: 0.4em;
+    left: 0.4em;
+
+    padding: 0.5em;
+
+    border-radius: 8px;
   }
 
   .category__item__details {
