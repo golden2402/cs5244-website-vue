1b45c31f21215549e50ce55327a3e7e30b9f5b31
golden2402 <golden2402@vt.edu>
2023-10-24 19:20:58 -0400

    add view components for cart & checkout

---
+++ src/views/CartView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="cart_page container">
+    <h1>Your Cart</h1>
+    <!-- cart component -->
+  </div>
+</template>
+
+<style scoped>
+  .cart_page > * {
+    flex: 1;
+    padding: 1em;
+  }
+</style>
---
+++ src/views/CheckoutView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="checkout_page container flex">
+    <h1>Checkout</h1>
+    <!-- checkout component -->
+  </div>
+</template>
+
+<style scoped>
+  .checkout_page > * {
+    flex: 1;
+    padding: 4em;
+  }
+</style>
