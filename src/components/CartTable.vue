f7fd735225e51795c3a1fe3c8ec778b8e90d1864
golden2402 <golden2402@vt.edu>
2023-10-25 00:56:36 -0400

    edit cart item display, add remove icon

--- src/components/CartTable.vue
+++ src/components/CartTable.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  import IconClose from "@/assets/icons/IconClose.vue";
   import BaseCard from "./BaseCard.vue";
 </script>
 
@@ -20,25 +21,19 @@
             <img class="cart__item__cover" src="/covers/1001.png" />
           </BaseCard>
         </div>
-        <p>The C Programming Language</p>
-        <input type="number" value="1" />
-        <div class="flex flex--column justify--center">$549</div>
-        <div class="flex flex--column justify--center align--center gap--sm">
-          <p>X</p>
+        <div class="flex flex--column justify--center">
+          <p class="cart__item__name">The C Programming Language</p>
         </div>
-      </div>
-
-      <div class="cart__item row">
-        <div class="flex justify--center">
+        <div class="flex flex--column justify--center">
           <BaseCard>
-            <img class="cart__item__cover" src="/covers/1001.png" />
+            <input class="cart__item__quantity" type="number" value="1" />
           </BaseCard>
         </div>
-        <p>The C Programming Language</p>
-        <input type="number" value="1" />
         <div class="flex flex--column justify--center">$549</div>
         <div class="flex flex--column justify--center align--center gap--sm">
-          <p>X</p>
+          <button class="remove__item__button">
+            <IconClose class="remove__item__icon" />
+          </button>
         </div>
       </div>
     </section>
@@ -52,24 +47,33 @@
 
   .table__widget .row {
     display: grid;
-    /* FIXME: replace ALL of these with minmax values: */
-    grid-template-columns: minmax(8em, 16em) minmax(8em, 16em) minmax(8em, 8em) minmax(8em, 16em) minmax(
-        8em,
-        16em
-      );
+    /* TODO: may need to figure out how to size these wrt. different screens: */
+    grid-template-columns: 10em 24em 8em 8em 4em;
+    gap: 1em;
   }
 
   .cart__item {
     border-radius: var(--default-border-radius);
-    padding: 0.8em 0;
+    padding: 0.75em 0;
   }
 
   .cart__item:nth-child(odd) {
-    /* background-color: var(--secondary--color); */
     background-color: var(--secondary-color);
   }
 
   .cart__item__cover {
-    max-height: 10em;
+    max-height: 8em;
+  }
+
+  .cart__item__quantity {
+    width: 100%;
+  }
+
+  .remove__item__button {
+    display: inline-flex;
+  }
+
+  .remove__item__icon {
+    color: var(--text-color-mute);
   }
 </style>
